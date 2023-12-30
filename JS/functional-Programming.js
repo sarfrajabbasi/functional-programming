f5dfa2f617de56e77855ae52212313ab8a58b563
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-30 17:11:51 +0530

    functional programming

---
+++ JS/functional-Programming.js
@@ -0,0 +1,75 @@
+/* basic concept of functional programming and core ideas which make us life long functional programmer.
+
+Q. What is functional programming and why are people so excited about it?
+
+Q. What does functional code look like? how is different from imperative or OO code?
+
+
+<==== LEARN=====>
+
+  Q. What will we learn in this course?
+
+Ans ==> we will learn about basic principle of the functional programming paradigm,such as:
+
+   --> we gonna talk about pure functions and side-effects why want to avoid them.
+   -->we gonna talk about programms as piplines through which data flows between functions.
+   --> talk about higher order functions,first class functions,closure,currying.our focus is not gonna be on the terminology so much,but those core ideas.
+   --> talk about immutablity and changing data in a way that's functional, that works  with functional programming.
+
+   {----
+-> construct programs from pure, “input in-output out” functions through which data flows.
+
+-> use higher-order functions like map & filter instead of iterative loops, and closures instead of objects & classes.
+
+-> avoid mutation (changing things in-place), and use immutable data structures for efficiency
+-----}
+
+=========== Outline/content for that i learn ===========
+
+1. Q. What is functional programming?
+
+    -> Pure Functions vs. Side Effects
+    -> Time, state, and (in)sanity
+    -> Imperative vs. Declarative programming
+
+2. Staying out of the loop with recursion
+    -> Iteration vs. Recursion
+    -> Recursive function basics
+    -> Exercise: Iteration vs. Recursion
+    -> The problem with the problem with the problem with recursion.
+
+ 3. Flying first-class with higher-order functions
+      -> Functions as values
+      -> Functions that operate on functions
+      -> Exercise: Filter, Map, Reduce.
+  
+ 4. Getting closure
+      -> Closures & "remembering" scope
+      -> Partial application
+      -> Currying
+      -> Exercise: Closures & Currying.
+
+ 5.  It's functions all the way down
+      -> Data flow
+      -> Pipelining
+      -> Function composition
+      -> Exercise: Function Composition
+  
+ 6.   Immutability
+      -> Time, state, and (in)sanity, revisited
+      -> Copying vs. Mutating
+      -> The pitfalls of immutability
+      -> Exercise: Avoiding Mutation    
+*/
+
+
+// ================= START HERE =================
+
+/* 
+
+Q. what is functional programming?
+
+
+
+*/
+
