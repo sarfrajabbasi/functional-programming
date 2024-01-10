90ac471e52a72040873cfa6eebeaa5103c5b25c3
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-01-10 22:48:00 +0530

    functional programming paradigm

--- JS/recursion.js
+++ JS/recursion.js
@@ -52,6 +52,12 @@ console.log(factorial1);
 // Fibonacci:---
 
 function iterativeFibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
   let a = 0;
   let b = 1;
   let c = 0;
