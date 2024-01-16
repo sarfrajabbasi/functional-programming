705818ded92ae124f0f4c1dea433b9e7f525d98d
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-01-16 17:25:18 +0530

    functional programming paradigm

--- JS/closure.js
+++ JS/closure.js
@@ -31,3 +31,28 @@ console.log(greetItal('ibola'));
 console.log(greetItal('corona'));
 console.log(greetItal('black death'));
 console.log(greetItal('bird flu')); 
+
+// function customFlat(arr,result=[]){
+//     arr.forEach((element)=>{
+//         if(Array.isArray(element)){
+//             result = result.concat(customFlat(element))
+//         }else{
+//             result.push(element)
+//         }
+//     })
+//     return result
+// }
+
+// function createNestedArray(depth, initialValue) {
+//     if (depth === 0) {
+//         return initialValue;
+//     } else {
+//         return createNestedArray(depth - 1, [initialValue]);
+//     }
+// }
+
+// const nestedArray = createNestedArray(10, [12,3,[1,2,3],[12,3,4,4,[1,2,3,[6,5,4]]]]);
+// console.log(nestedArray);
+// console.log(customFlat([12,3,[1,2,3],[12,3,4,4,[1,2,3,[6,5,4]]]]));
+// console.log(customFlat(nestedArray));
+
